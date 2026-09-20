0cd1e4ce48ec530958d40d59498ea4cb0d90a987
suryajithv <suryajithvarayath5@gmail.com>
2026-09-20 23:51:30 +0530

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,136 @@
+#1
+print("Hello World!.")
+print("Welcome to Python.")
+print("Suryajith V")
+print("I am learning Python.")
+print("Python Programming")
+print("Good Morning.")
+print("Python is easy.")
+print("My First Program")
+print("Hello from Python!.")
+
+#2
+print("2 + 5")
+print("2 + 5 =", 2 + 5)
+print("10 - 3")
+print(10 - 3)
+print("Hello World!")
+print("Python")
+print("Hello")
+print("Hello.")
+print("10 + 20")
+print(10 + 20)
+
+#3
+name ="suryajith" 
+print(name)
+
+age=20
+print(age)
+
+marks=85
+print(marks)
+
+price=99.5
+print(price)
+
+city="Chennai"
+print(city)
+
+score=95
+print(score)
+
+x=10
+print(x)
+
+message="Welcome"
+print(message)
+
+a=10
+b=20
+print(a+b)
+
+quantity=5
+price=10
+print(quantity * price)
+
+#4
+print(type(25))
+
+print(type(25.5))
+
+print(type("25"))
+
+age=21
+print("age is",age)
+
+height=5.8
+print("height is",height)
+
+name="Arun"
+print("name is",name)
+
+age=20
+price=99.5
+name="Ravi"
+print("age is",age)
+print("price is",price) 
+print("name is",name)
+
+marks=90
+print(type(marks))
+
+temperature=36.5
+print(type(temperature))
+
+message="Welcome"
+print(type(message))
+
+#5
+print("start")
+print("Middle")
+print("End")
+
+
+print("First")
+print("Second")
+print("Third")
+
+x=10
+print(x)
+x=20
+print(x)
+
+number=5
+print(number)
+number=10
+print(number)
+
+print("A")
+print("B")
+print("C")
+
+name="Ravi"
+age=20
+print("Name:",name)
+print("Age:",age)
+
+print("1")
+print("2")
+print("3")
+print("4")
+
+x=5
+y=10
+print("x:",x)
+print("y:",y)
+
+print("Start")
+print(2 + 3)
+print("End")
+
+print("Login")
+print("Dashboard")
+print("Logout")
+
+
